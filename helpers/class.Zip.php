9ba91b5bb5694a12bba0ac40140628a7234e5d90
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-08-19 18:48:09 +0000

    fix .svn include
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@15449 62dfeb86-a93f-4914-9d13-15c0a160985d

--- helpers/class.Zip.php
+++ helpers/class.Zip.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,34 +23,47 @@
  * @subpackage helpers
  *
  */
+class taoUpdate24_helpers_Zip
+{
 
-class taoUpdate24_helpers_Zip{
-
-  
-    
-    public static function addFile($src, $dest) {
+    public static function addFile($src, $dest)
+    {
         
-        //todo check right
+        // todo check right
         $zip = new ZipArchive();
         
-         if($zip->open($dest, ZipArchive::CREATE) !== true){
-            throw new Exception('Unable to create archive at '.$dest);
+        if ($zip->open($dest, ZipArchive::CREATE) !== true) {
+            throw new Exception('Unable to create archive at ' . $dest);
         }
-        
-        $all= new RecursiveIteratorIterator(new RecursiveDirectoryIterator($src));
-        
-        foreach ($all as $f=>$value) {
+        $src = str_replace('\\', '/', realpath($src));
+        if (is_dir($src) === true) {
             
-            if(strpos($f, '.svn') != false) {
-                continue;
+            $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($src));
+                        
+            foreach ($files as $file) {
+                $file = str_replace('\\', '/', $file);
+                // Ignore "." ".." ".svn" folders
+                if (in_array(substr($file, strrpos($file, '/') + 1), array(
+                    '.',
+                    '..'
+                )) || strrpos($file, '/.svn/')) {
+                    continue;
+                }
+                var_dump($file);
+                $file = realpath($file);
+                
+                if (is_dir($file) === true) {
+                    $zip->addEmptyDir(str_replace($src . '/', '', $file . '/'));
+                } else 
+                    if (is_file($file) === true) {
+                        $zip->addFromString(str_replace($src . '/', '', $file), file_get_contents($file));
+                    }
             }
-           
-            $zip->addFile(realpath($f), $f) or die ("ERROR: Unable to add file: $f");
-        }
-        $zip->close();
-
+        } else 
+            if (is_file($src) === true) {
+                $zip->addFromString(basename($src), file_get_contents($src));
+            }
+        
+        return $zip->close();
     }
-    
-    
-
 }
