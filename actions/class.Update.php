6791101e1fc667dea871d4b5d56a8e25156cb79a
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-13 12:47:15 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@16107 62dfeb86-a93f-4914-9d13-15c0a160985d

--- actions/class.Update.php
+++ actions/class.Update.php
@@ -156,7 +156,7 @@ class taoUpdate_actions_Update extends tao_actions_CommonModule {
         $this->setData('isDesignModeEnabled', $isDesignModeEnabled);
         $this->setData('isUpdateAvailable', $isUpdateAvailable);
         $this->setData('hasProperRole', $hasProperRole);
-        $successMsg =  __('New Version have been downloaded and will now be extracted, we will now replace current installation');
+        $successMsg =  __('New Version have been downloaded and will now be extracted, we will now replace current installation. ');
         $successUrl = ROOT_URL .taoUpdate_models_classes_Service::DEPLOY_FOLDER . 'Main/index?key=' . $this->service->getKey();  
         
         
