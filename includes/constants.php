0023a7d4570b4c922eb0605dfbc5e7932dd6a873
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2013-09-13 10:17:35 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@16086 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,11 @@
 <?php
 
 $todefine = array(
-    'RELEASES_MANIFEST'     => BASE_URL . '/test/sample/releases10.xml',
+    //for 2.4
+    'RELEASES_MANIFEST'     => 'http://releases.taotesting.com/releases24.xml',
+    //for testcase
+    //'RELEASES_MANIFEST'     => BASE_URL . '/test/sample/releases10.xml',
+     
     "UPDATE_SITE_IS_LOCAL" => true,
 
 );
