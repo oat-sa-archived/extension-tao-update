4067a3c6650d44a480e6b0f7f5118f2f9cd17c87
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-03 14:42:21 +0000

    remove unused file from previous version 2.4 2.5
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@18832 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/install/addSysAdminRoleToSuperUser.php
+++
@@ -1,6 +0,0 @@
-<?php
-common_Logger::d('Add SysAdminRole to '.LOCAL_NAMESPACE. '#superUser');
-$superUser = new core_kernel_classes_Resource(LOCAL_NAMESPACE.'#superUser');
-$property = new core_kernel_classes_Property('http://www.tao.lu/Ontologies/generis.rdf#userRoles');
-$superUser->setPropertyValue($property, 'http://www.tao.lu/Ontologies/TAO.rdf#SysAdminRole');
-
