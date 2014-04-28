d00f14c37aa1006b99c90e87325e87ecb0995547
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-28 12:09:56 +0000

    adapted ACL interface order
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@19945 62dfeb86-a93f-4914-9d13-15c0a160985d

--- models/classes/class.CustomAccess.php
+++ models/classes/class.CustomAccess.php
@@ -39,7 +39,7 @@ class taoUpdate_models_classes_CustomAccess
      * (non-PHPdoc)
      * @see tao_models_classes_accessControl_AccessControl::hasAccess()
      */
-    public function hasAccess($extension, $controller, $action, $parameters) {
+    public function hasAccess($action, $controller, $extension, $parameters) {
         if($extension == 'taoUpdate' && $controller = 'data'){
             return true;
         }else {
