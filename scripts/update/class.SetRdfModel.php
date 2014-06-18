d63a388990e3df61a81794e2508be3a320e2ab13
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-18 14:24:00 +0000

    set data model
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@20930 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/update/class.SetRdfModel.php
+++ scripts/update/class.SetRdfModel.php
@@ -1,7 +1,7 @@
 <?php
 use oat\generis\model\data\ModelManager;
 
-class taoUpdate_scripts_update_setRdfModel
+class taoUpdate_scripts_update_SetRdfModel
 {
     public function run(){
         ModelManager::setModel(new \core_kernel_persistence_smoothsql_SmoothModel(array('persistence' => 'default')));
