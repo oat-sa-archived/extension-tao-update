e3a22ead16939d9673018406a9f66e9c21c7a334
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-26 15:11:23 +0000

    add filesource
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@21067 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ scripts/update/class.SertupServiceFileStorage.php
@@ -0,0 +1,28 @@
+<?php
+namespace update;
+
+class taoUpdate_scripts_update_SertupServiceFileStorage extends tao_scripts_Runner
+{
+    public function run() {
+
+        
+        $publicDataPath = FILES_PATH.'tao'.DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR;
+        $privateDataPath = FILES_PATH.'tao'.DIRECTORY_SEPARATOR.'private'.DIRECTORY_SEPARATOR;
+        
+        if (file_exists($publicDataPath)) {
+            helpers_File::emptyDirectory($publicDataPath);
+        }
+        if (file_exists($privateDataPath)) {
+            helpers_File::emptyDirectory($privateDataPath);
+        }
+        
+        $publicFs = tao_models_classes_FileSourceService::singleton()->addLocalSource('public service storage', $publicDataPath);
+        $privateFs = tao_models_classes_FileSourceService::singleton()->addLocalSource('private service storage', $privateDataPath);
+        
+        $provider = tao_models_classes_fsAccess_TokenAccessProvider::spawnProvider($publicFs);
+        
+        tao_models_classes_service_FileStorage::configure($privateFs, $publicFs, $provider);
+    }
+}
+
+?>
