d799a6b3018ed08d10c6f3f61068934c72dc42c2
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-26 13:32:31 +0000

    error in update
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@21063 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/update/class.SetRdfModel.php
+++ scripts/update/class.SetRdfModel.php
@@ -1,7 +1,7 @@
 <?php
 use oat\generis\model\data\ModelManager;
 
-class taoUpdate_scripts_update_SetRdfModel
+class taoUpdate_scripts_update_SetRdfModel extends tao_scripts_Runner
 {
     public function run(){
         ModelManager::setModel(new \core_kernel_persistence_smoothsql_SmoothModel(array('persistence' => 'default')));
