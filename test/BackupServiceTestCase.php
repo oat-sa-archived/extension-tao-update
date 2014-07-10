33a5295c6e87187bc772b4569191ee6605963060
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-07-10 12:25:40 +0000

    revert 2.5 still use simpletest
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@21371 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ test/BackupServiceTestCase.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ *
+ */
+
+require_once dirname(__FILE__) . '/../../tao/test/TaoTestRunner.php';
+require_once dirname(__FILE__) . '/../includes/raw_start.php';
+
+/**
+ * @license GPLv2
+ * @package taoUpdate
+ * @subpackage models_classes
+ * @author "Lionel Lecaque, <lionel@taotesting.com>"
+ *
+ */
+class BackupServiceTestCase extends UnitTestCase {
+    /**
+     * 
+     * @var taoUpdate_models_classes_BackupService
+     */
+    private $service;
+    /**
+     * 
+     * @var string
+     */
+    private $folder;
+    
+    /**
+     * tests initialization
+     */
+    public function setUp(){
+        TaoTestRunner::initTest();
+        $this->service = taoUpdate_models_classes_BackupService::singleton();
+
+    }
+    
+    public function testCreate() {
+        try {
+            $this->folder = $this->service->createBackupFolder();
+            $this->service->storeAllFiles($this->folder);
+            $srcFile = $this->folder . DIRECTORY_SEPARATOR.
+                    taoUpdate_models_classes_BackupService::SRC_BACKUP_FILE_PREFFIX. 
+                    TAO_VERSION.
+                    taoUpdate_models_classes_BackupService::SRC_BACKUP_FILE_SUFFIX;
+            $this->assertTrue(is_file($srcFile));
+            $this->service->storeDatabase($this->folder);
+            $dbFile = $this->folder . DIRECTORY_SEPARATOR.
+                    taoUpdate_models_classes_BackupService::DB_BACKUP_FILE_PREFFIX.
+                    TAO_VERSION.
+                    taoUpdate_models_classes_BackupService::DB_BACKUP_FILE_SUFFIX.
+                    '.zip';
+			
+            $this->assertTrue(is_file($dbFile));
+        }
+        catch(taoUpdate_models_classes_UpdateException $e){
+            $this->fail('Exception raised ' . $e->getMessage());
+        }
+
+    }
+    
+    public function tearDown(){
+        helpers_File::remove($this->folder);
+    }
+    
+    
+}
---
+++ test/ZipTestCase.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ *
+ */
+
+require_once dirname(__FILE__) . '/../../tao/test/TaoTestRunner.php';
+require_once dirname(__FILE__) . '/../includes/raw_start.php';
+
+/**
+ * @license GPLv2
+ * @package taoUpdate
+ * @subpackage models_classes
+ * @author "Lionel Lecaque, <lionel@taotesting.com>"
+ *
+ */
+class ZipTestCase extends UnitTestCase {
+
+
+    /**
+     * tests initialization
+     */
+    public function setUp(){
+        TaoTestRunner::initTest();
+
+    }
+
+    public function testCompressFile(){
+        $src = dirname(__FILE__).'/sample/releases.xml';
+        $dest = dirname(__FILE__).'/backup/releases.xml.zip';
+        taoUpdate_helpers_Zip::compressFile($src,$dest);
+        $zip = new ZipArchive();
+        $zip->open($dest);
+        $this->assertFalse($zip->locateName('releases.xml') === false);
+        helpers_File::remove($dest);
+    }
+    
+    public function testCompressFolder() {
+        $files = array (
+            'releases-noNewPatch.xml',
+            'releases-patchsOnly.xml',
+            'releases.xml',
+            'folder/',
+            'folder/emptyFile',
+            'emptyFolder/',
+        );
+        $dest = dirname(__FILE__).'/backup/test.zip';
+        $src = dirname(__FILE__).'/sample';
+        taoUpdate_helpers_Zip::compressFolder($src,$dest);
+        $this->assertTrue(is_file($dest));
+        $zip = new ZipArchive();      
+        $zip->open($dest);
+        foreach ($files as $file){
+            $this->assertFalse($zip->locateName($file) ===false,$file . ' not found');
+        }
+        $this->assertFalse($zip->locateName('.svn'));
+        helpers_File::remove($dest);
+        $dest = dirname(__FILE__).'/backup/test2.zip';
+        taoUpdate_helpers_Zip::compressFolder($src,$dest,true);
+        $files = array (
+            'sample/releases-noNewPatch.xml',
+            'sample/releases-patchsOnly.xml',
+            'sample/releases.xml',
+            'sample/folder/',
+            'sample/folder/emptyFile',
+            'sample/emptyFolder/',
+        );
+        $this->assertTrue(is_file($dest));
+        $zip = new ZipArchive();
+        $zip->open($dest);
+        for( $i = 0; $i < $zip->numFiles; $i++ ){
+            $stat = $zip->statIndex( $i );
+            //cehck no .svn added in zip
+            $this->assertFalse(strpos($stat['name'], '.svn') > 0);
+        
+        }
+        foreach ($files as $file){
+            $this->assertFalse($zip->locateName($file) === false);
+        }
+        helpers_File::remove($dest);
+
+    }
+    
+    
+}
