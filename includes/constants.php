a4d429b68bb96c32f73f0fe89960f29fd2fa7653
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-07-14 12:36:41 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@21420 62dfeb86-a93f-4914-9d13-15c0a160985d

--- includes/constants.php
+++ includes/constants.php
@@ -5,7 +5,7 @@ $todefine = array(
     'RELEASES_MANIFEST'     => 'http://releases.taotesting.com/releases25.xml',
     //for testcase
     'RELEASES_LOCAL_MANIFEST'     => BASE_DATA . '/local/releases.xml',
-     
+    'INSTANCE_ROLE_SYSADMIN'			=> 'http://www.tao.lu/Ontologies/TAO.rdf#SysAdminRole',
     "UPDATE_SITE_IS_LOCAL" => true,
 
 );
