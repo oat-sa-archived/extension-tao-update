3a3d5383e1886c9627a47ef3b2885ed3696214ef
llecaque <llecaque@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-07-28 14:00:01 +0000

    bad extension installer in script
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoUpdate/trunk@21551 62dfeb86-a93f-4914-9d13-15c0a160985d

--- scripts/update/class.InstallNewExtension.php
+++ scripts/update/class.InstallNewExtension.php
@@ -26,7 +26,7 @@
 class taoUpdate_scripts_update_InstallNewExtension extends tao_scripts_Runner {
     
     public function run(){
-    	$extmanger = common_ext_ExtensionsManager::singleton();
+    	$extmanger = tao_install_ExtensionInstaller::singleton();
         $ext = $extmanger->getExtensionById('taoCe');
         $extinstaller = new common_ext_ExtensionInstaller($ext);
         $extinstaller->install();
