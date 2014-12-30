1937f51c71cea7b1af63fd4109e8a83018994afe
Lionel Lecaque <lionel@taotesting.com>
2014-12-30 15:56:10 +0100

    set manifest

--- includes/constants.php
+++ includes/constants.php
@@ -1,8 +1,8 @@
 <?php
 
 $todefine = array(
-    //for 2.4
-    'RELEASES_MANIFEST'     => 'http://releases.taotesting.com/releases25.xml',
+    //for 2.6 to 3.0
+    'RELEASES_MANIFEST'     => 'http://releases.taotesting.com/releases26.xml',
     //for testcase
     'RELEASES_LOCAL_MANIFEST'     => BASE_DATA . '/local/releases.xml',
     "UPDATE_SITE_IS_LOCAL" => true,
